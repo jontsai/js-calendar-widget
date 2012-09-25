553d9b180b5236be2767e435b89d24acab4c5532
Jonathan Tsai <jontsai@jonathantsai.com>
2012-09-25 23:54:41 +0000

    use braces for inline php variables, not parens

--- calendar_wrapper.php
+++ calendar_wrapper.php
@@ -6,9 +6,9 @@ function render_calendar($rel_path = "./") {
 
   // use echoes instead of inline PHP, which is not allowed by all Apache configs
   echo('<script src="http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js"></script>');
-  echo("<script src=\"$(rel_path)calendar_settings_default.js\"></script>");
-  echo("<script src=\"$(rel_path)helpers.js\"></script>");
-  echo("<script src=\"$rel_path)calendar.js\"></script>");
+  echo("<script src=\"${rel_path}calendar_settings_default.js\"></script>");
+  echo("<script src=\"${rel_path}helpers.js\"></script>");
+  echo("<script src=\"${rel_path}calendar.js\"></script>");
 
   include('calendar_markup.html');
   chdir($_orig_dir);
