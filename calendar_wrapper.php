2c19e74a9bd8ea7e9b313f71fec5c634ddea40ee
Jonathan Tsai <jontsai@jonathantsai.com>
2012-09-25 23:50:19 +0000

    use echoes in favor of inline php, which is not allowed in some Apache configs

--- calendar_wrapper.php
+++ calendar_wrapper.php
@@ -3,12 +3,13 @@
 function render_calendar($rel_path = "./") {
   $_orig_dir = getcwd();
   chdir(dirname(__FILE__));
-?>
-<script src="http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js"></script>
-<script src="<?= $rel_path ?>calendar_settings_default.js"></script>
-<script src="<?= $rel_path ?>helpers.js"></script>
-<script src="<?= $rel_path ?>calendar.js"></script>
-<?php
+
+  // use echoes instead of inline PHP, which is not allowed by all Apache configs
+  echo('<script src="http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js"></script>');
+  echo("<script src=\"$(rel_path)calendar_settings_default.js\"></script>");
+  echo("<script src=\"$(rel_path)helpers.js\"></script>");
+  echo("<script src=\"$rel_path)calendar.js\"></script>");
+
   include('calendar_markup.html');
   chdir($_orig_dir);
 
